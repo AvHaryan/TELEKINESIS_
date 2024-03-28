9fae5e721253953e9600a311dc4cfa34cb001e62
Avo <avoharutyunyan3@gmail.com>
2024-03-28 17:19:43 +0400

    initial commit

---
+++ leetcode/problem_20.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(std::string s) {
+        std::stack<char> stack;
+
+        for (char& c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.empty()){
+                     return false;
+                }
+                char top = stack.top();
+                if ((c == ')' && top != '(') 
+                 || (c == '}' && top != '{') 
+                 || (c == ']' && top != '[')) {
+                 return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.empty();
+    }
+};
